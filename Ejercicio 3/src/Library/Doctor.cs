a8d885072af48c982e27c841f250f4f8ffdfda79
Nicolas <nicolas.lozano.282002@gmail.com>
2020-09-04 19:56:01 -0300

    Modificacion

---
+++ Ejercicio 3/src/Library/Doctor.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Text;
+
+namespace InformacionDoctor
+{
+    public class Doctor
+    {
+        public static string CreateAppointment(string name, string id, string phoneNumber, string doctorName)
+        {
+            StringBuilder stringBuilder = new StringBuilder("Scheduling appointment...\n");
+            Boolean isValid = true;
+
+            if (string.IsNullOrEmpty(id))
+            {
+                stringBuilder.Append("Unable to schedule appointment, id is required\n");
+                isValid = false;
+            }
+
+            if (string.IsNullOrEmpty(phoneNumber))
+            {
+                stringBuilder.Append("Unable to schedule appointment, Phone number is required\n");
+                isValid = false;
+            }
+            
+            if (string.IsNullOrEmpty(doctorName))
+            {
+                stringBuilder.Append("Unable to schedule appointment, Doctor name is required\n");
+                isValid = false;
+            }
+
+            if (isValid)
+            {
+                stringBuilder.Append("Appoinment Scheduled");
+            }
+
+            return stringBuilder.ToString();
+        }
+
+    }
+}
